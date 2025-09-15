7c21f5b2594dd847383ab21b8b45a5de8f350332
dmm76 <douglas.monquero@gmail.com>
2025-09-15 15:42:06 -0300

    chore: ignore caches e arquivos gerados

--- storage/tmp/phpstan/cache/PHPStan/53/c9/53c9ef243e90a665147a8209eb554c06a0c06b13.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types = 1);
-
-// variadic-method-App\DAO\MarcaDAO-__construct-C:\Apache24\Apache24\htdocs\projeto_mercadinho_web\app\DAO\MarcaDAO.php
-return \PHPStan\Cache\CacheItem::__set_state(array(
-   'variableKey' => '1757542585-v4',
-   'data' => false,
-));
