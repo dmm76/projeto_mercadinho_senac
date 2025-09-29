efc12547d9ccd86c76c09b01f7f38d1c9b3d0083
dmm76 <douglas.monquero@gmail.com>
2025-09-29 15:45:10 -0300

    convertendo imagens do carrosel

--- App/Views/site/home/index.php
+++ App/Views/site/home/index.php
@@ -84,27 +84,27 @@
 
                         <div class="carousel-item">
 
-                            <img src="<?= $ASSETS ?>/banca.jpg" class="d-block w-100 carousel-img" alt="Frutas frescas">
+                            <img src="<?= $ASSETS ?>/banca.webp" class="d-block w-100 carousel-img" alt="Frutas frescas">
 
                         </div>
 
                         <div class="carousel-item">
 
-                            <img src="<?= $ASSETS ?>/higiene.jpg" class="d-block w-100 carousel-img"
+                            <img src="<?= $ASSETS ?>/higiene.webp" class="d-block w-100 carousel-img"
                                 alt="Produtos de Higiene">
 
                         </div>
 
                         <div class="carousel-item">
 
-                            <img src="<?= $ASSETS ?>/paes.jpg" class="d-block w-100 carousel-img"
+                            <img src="<?= $ASSETS ?>/paes.webp" class="d-block w-100 carousel-img"
                                 alt="Pães fresquinhos">
 
                         </div>
 
                         <div class="carousel-item">
 
-                            <img src="<?= $ASSETS ?>/compras.jpg" class="d-block w-100 carousel-img" alt="Promocoes">
+                            <img src="<?= $ASSETS ?>/compras.webp" class="d-block w-100 carousel-img" alt="Promocoes">
 
                         </div>
 
